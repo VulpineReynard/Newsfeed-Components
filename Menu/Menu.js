dc9ed11f5f2f74e17d51ad69329172f59ece51fb
VulpineReynard <milare8@gmail.com>
2019-07-10 16:57:39 -0400

    added animation to menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -19,17 +19,46 @@ let menuItems = [
     </ul>
   </div>
 
+*/
+// Step 6: add the menu component to the DOM.
+document.querySelector('.header').appendChild(createMenu(menuItems));
+
+function createMenu(menuData) {
+  const menu = document.createElement('div');
+  const menuList = document.createElement('ul');
+
+  menu.appendChild(menuList);
+  menu.classList.add('menu');
+
+  // Step 2: Iterate over the array creating a list item <li> element for each item in the array. Add those items to the <ul>
+  menuData.forEach(data => {
+    let menuItem = document.createElement('li');
+    menuItem.textContent = data;
+    menuList.appendChild(menuItem);
+  });
+
+  // Step 3: Using a DOM selector, select the menu button currently on the DOM.
+  const menuButton = document.querySelector('.menu-button');
+
+  // Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  menuButton.addEventListener('click', event => {
+    if(menu.style.left !== '0px') {
+      menu.classList.toggle('menu--open');
+      TweenMax.to(menu, .5, {left: '0px'});
+    } else {
+      TweenMax.to(menu, .5, {left: '-350px'});
+      setTimeout(() => {
+        menu.classList.toggle('menu--open');
+      }, 500);
+    }
+  });
+
+  // Step 5: return the menu component.
+  return menu;
+}
+/*
+
   Pass the function an array as it's only argument.
-
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
   
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
   
 */
